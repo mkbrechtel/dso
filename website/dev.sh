3a52f07fc3c7b22af6400657fd7757a04311c3b5
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2024-08-19 09:50:33 +0200

    update dev.sh script

--- website/dev.sh
+++ website/dev.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
-exec podman run -it -v .:/app -v ../docs:/docs --workdir /app -p 4321:4321 --name dso-website --hostname dso-website node:20 sh -c  'yarn dev --host & exec bash'
+docker rm -f dso-website
+exec docker run -it -v .:/app -v ../docs:/docs --workdir /app -p 4321:4321 --name dso-website --hostname dso-website node:20 sh -c  'yarn install & yarn dev --host & exec bash'
